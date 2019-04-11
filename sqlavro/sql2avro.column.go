ad8aaa380c39183e5e8c5f44c36f6671808f0c70
Guillaume Simonneau <2980507+khezen@users.noreply.github.com>
2019-04-11 23:44:46 +0200

    coverage

--- sqlavro/sql2avro.column.go
+++ sqlavro/sql2avro.column.go
@@ -91,6 +91,9 @@ func sqlColumn2AVRO(columnName string, dataType SQLType, isNullable bool, defaul
 				format = "2006-01-02 15:04:05"
 			}
 			t, err := time.Parse(format, string(defaultValue))
+			if dataType == Time {
+				t = t.AddDate(1970, 0, 0)
+			}
 			if err != nil {
 				defaultValue = []byte(fmt.Sprintf(`"%s"`, string(defaultValue)))
 			} else {
