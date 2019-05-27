77c2a9dfc0b99ca5bd2150425bae4b46112a5542
Guillaume Simonneau <=>
2019-05-27 13:29:31 +0200

    fix aliases query matching

--- sqlavro/criterion.type.go
+++ sqlavro/criterion.type.go
@@ -13,7 +13,7 @@ func ensureCriterionTypes(schema *avro.RecordSchema, criteria []Criterion) (err 
 	for i = range criteria {
 		match = false
 		for _, field = range schema.Fields {
-			match = criteria[i].FieldName == field.Name || (len(field.Aliases) > 0 && field.Aliases[0] == field.Name)
+			match = criteria[i].FieldName == field.Name || (len(field.Aliases) > 0 && field.Aliases[0] == criteria[i].FieldName)
 			if match {
 				criteria[i].setSchema(field)
 				break
